37629e8ae0c00df104aa0654b88fc8d26f196040
Borja <borjanana23@gmail.com>
2023-03-31 22:17:02 +0200

    Commit Refactorizado

--- src/Coche.java
+++ src/Coche.java
@@ -3,11 +3,11 @@ public class Coche {
 
 	public int velocidad;
 
-	public void acelerar(int aceleracion) {
+	public void acelerar_Borja(int aceleracion) {
 		velocidad += aceleracion;
 	}
 
-	public void decelerar(int deceleracion) {
+	public void decelerar_Borja(int deceleracion) {
 		velocidad -= deceleracion;
 		if (velocidad <0) velocidad = 0;
 	}
